08adc57c6154b6c8b295bc758e2b9c1981381f78
tamanghawana0107 <hawanatamang@gmail.com>
2022-03-16 23:14:02 +0545

    added

--- Large number.cpp
+++ Large number.cpp
@@ -1,3 +1,37 @@
 //WAP to find the largest number of two class using friend function.
+#include<iostream>
+using namespace std;
+class num2;
+class num1{
+	int a;
+	public:
+		void getDetails(int x)
+		{
+			a=x;
+		}
+		friend class num2;
+};
+class num2{
+	int b;
+	public: 
+	void getDetails( int y)
+	{
+		b=y;
+	}
+	friend void largest();
+};
 
-
+int main
+{
+	num1 obj1;
+	num2 obj2,obj3;
+	int h,r;
+	cout<<"Enter the first number:";
+	cin>>h;
+	cout<<"Enter the second number:";
+	cin>>r;
+	obj1.getDetails(h);
+	obj2.getDetails(r);
+	
+	return 0;
+}
